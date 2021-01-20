27587a8e8a28d77cfa92c63d388d2d26fe03ba71
Walter Fernandes Justino <walterfjustino@gmail.com>
2021-01-19 21:51:23 -0300

    Developed project entities

---
+++ src/main/java/com/api/bookstoremanager/entity/Author.java
@@ -0,0 +1,28 @@
+package com.api.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+
+}
