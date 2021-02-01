72b96fdc6e6b3504d169d9abb228bde3e1cf7d72
Walter Fernandes Justino <walterfjustino@gmail.com>
2021-02-01 12:43:00 -0300

    Developed get by id operation

--- src/main/java/com/api/bookstoremanager/controller/BookController.java
+++ src/main/java/com/api/bookstoremanager/controller/BookController.java
@@ -4,10 +4,7 @@ import com.api.bookstoremanager.dto.BookDTO;
 import com.api.bookstoremanager.dto.MessageResponseDTO;
 import com.api.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,9 +23,13 @@ public class BookController {
 
     @PostMapping
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO){
-
         return bookService.create(bookDTO) ;
     }
 
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id){
+        return bookService.findById(id);
+
+    }
 
 }
