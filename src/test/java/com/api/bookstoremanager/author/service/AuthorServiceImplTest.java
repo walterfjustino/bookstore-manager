fceece7e753ddd59bd99abf37271e342bda1a170
Walter <walterfjustino@gmail.com>
2022-08-22 13:18:31 -0300

    feat: add test scenario if author name exists need throw exception

--- src/test/java/com/api/bookstoremanager/author/service/AuthorServiceImplTest.java
+++ src/test/java/com/api/bookstoremanager/author/service/AuthorServiceImplTest.java
@@ -3,6 +3,7 @@ package com.api.bookstoremanager.author.service;
 import com.api.bookstoremanager.author.builder.AuthorDTOBuilder;
 import com.api.bookstoremanager.author.dto.AuthorDTO;
 import com.api.bookstoremanager.author.entity.Author;
+import com.api.bookstoremanager.author.exception.AuthorAlreadyExistsException;
 import com.api.bookstoremanager.author.mapper.AuthorMapper;
 import com.api.bookstoremanager.author.repository.AuthorRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +19,7 @@ import java.util.Optional;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 
@@ -41,6 +43,7 @@ public class AuthorServiceImplTest {
     }
 
     @Test
+    @DisplayName("criar um novo usuario, se não houver nenhum cadastrado com o mesmo nome")
     void when_New_Author_Is_Informed_Then_It_Should_Be_Created() {
         // @Given
         AuthorDTO expectedAuthorToCreateDTO = authorDtoBuilder.buildAuthorDTO();
@@ -53,5 +56,19 @@ public class AuthorServiceImplTest {
         AuthorDTO createdAuthorDTO = service.create(expectedAuthorToCreateDTO);
         // @Then
         assertThat(createdAuthorDTO, is(equalTo(expectedAuthorToCreateDTO)));
+    }
+
+    @Test
+    @DisplayName("Lança a exceção quando encontrado que o usuario a ser incluido já existe")
+    void when_Existing_Author_Is_Informed_Then_An_Exception_It_Should_Be_Throw() {
+        // @Given
+        AuthorDTO expectedAuthorToCreateDTO = authorDtoBuilder.buildAuthorDTO();
+        Author expectedCreatedAuthor = mapper.toModel(expectedAuthorToCreateDTO);
+
+        // @When
+        when(authorRepository.findByName(expectedAuthorToCreateDTO.getName())).thenReturn(Optional.of(expectedCreatedAuthor));
+
+         // @Then
+        assertThrows(AuthorAlreadyExistsException.class, ()-> service.create(expectedAuthorToCreateDTO));
     }
 }
