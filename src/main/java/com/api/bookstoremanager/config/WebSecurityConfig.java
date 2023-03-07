4848a104b9534505bf406f2431849eb8d57a36ad
walterfjustino <walterfjustino@gmail.com>
2023-03-07 18:05:42 -0300

    feature: add README.MD

--- src/main/java/com/api/bookstoremanager/config/WebSecurityConfig.java
+++ src/main/java/com/api/bookstoremanager/config/WebSecurityConfig.java
@@ -15,8 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Arrays;
-
 
 @Configuration
 @EnableWebSecurity
